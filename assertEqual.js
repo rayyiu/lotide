379b9c428f39b2805c14f62e46d437187bc49301
Ray <rayyiu66@gmail.com>
2021-02-01 16:22:32 -0800

    added assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("boot", "boot");
+assertEqual(3, 3);
+assertEqual(3, 6);
