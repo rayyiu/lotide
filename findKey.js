24c59dddd2147a1d7da7632dcff51daec8181127
Ray <rayyiu66@gmail.com>
2021-02-04 19:17:51 -0800

    added findKey.js to lotide

---
+++ findKey.js
@@ -0,0 +1,39 @@
+//worked on with brody bayley
+
+const findKey = function (object, callback) {
+    for (const key in object) {
+      // console.log(object[key])
+      if (callback(object[key])) {
+        return key;
+      }
+    }
+    //   // //if (callback returns true on object.key){
+    //   //   return true;
+    //   //   break;
+  }
+  
+  const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+      console.log(`❤️❤️❤️ Assertion Passed: ${actual} === ${expected}`);
+    } else if (actual !== expected) {
+      console.log(`🤷🏻‍♂️🤷🏻‍♂️🤷🏻‍♂️ Assertion Failed: ${actual} !== ${expected}`);
+    }
+    return;
+  };
+  findKey({
+    "Blue Hill": { stars: 1 },
+    "Akaleri": { stars: 3 },
+    "noma": { stars: 2 },
+    "elBulli": { stars: 3 },
+    "Ora": { stars: 2 },
+    "Akelarre": { stars: 3 }
+  }, x => x.stars === 2) // => "noma"
+  
+  assertEqual(findKey({
+    "Blue Hill": { stars: 1 },
+    "Akaleri": { stars: 3 },
+    "noma": { stars: 2 },
+    "elBulli": { stars: 3 },
+    "Ora": { stars: 2 },
+    "Akelarre": { stars: 3 }
+  }, x => x.stars === 2), "noma")
