61658d49d95ca891519eb9bef78becee5254a392
Ray <rayyiu66@gmail.com>
2021-02-08 15:59:59 -0800

    refactored tail

--- tail.js
+++ tail.js
@@ -1,9 +1,6 @@
 const assertEqual = require('./assertEqual')
-console.log('yay')
   
-const head = function(array) {
-  return array[0];
-};
+const head = require('head');
 
 const tail = function(array) {
   let tail = [];
@@ -12,13 +9,5 @@ const tail = function(array) {
   }
   return tail;
 };
-
-console.log(tail([1, 2, 3, 4, 5]));
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-console.log(assertEqual(result, ["Lighthouse", "Labs"]));
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log("tail,", tail(words));
-
-assertEqual(words.length, 3);
 
 module.export = tail;
---
+++ test/tailTest.js
@@ -0,0 +1,7 @@
+const tail = require("./tail");
+
+console.log(tail([1, 2, 3, 4, 5]));
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+console.log(assertEqual(result, ["Lighthouse", "Labs"]));
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+console.log("tail,", tail(words));
