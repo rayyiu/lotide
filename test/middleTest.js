99f01c4cdc150bf5874561bb8419e0bf414a4bfa
Ray <rayyiu66@gmail.com>
2021-02-08 19:27:27 -0800

    refactored middleTest

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,20 @@
-const assertArraysEqual = require("../assertArraysEqual.js");
+const assert = require('chai').assert;
 const middle = require("../middle.js")
 console.log(middle([1,2,3,4]));
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3]);
 
+
+describe("#middle", () => {
+    it("returns 2 for [1, 2, 3]", () => {
+      assert.deepEqual(middle([1, 2, 3]), [2]);
+    });
+    it("returns [] for ['5']", () => {
+      assert.deepEqual(middle(['5']), []); 
+    });
+    it("returns ['Lighthouse'] for [['Hello', 'Lighthouse', 'Labs']", () => {
+        assert.deepEqual(middle(["Hello", "Lighthouse", "Labs"]), ["Lighthouse"])
+    });
+    it("returns [2, 3] for [1, 2, 3, 4]", () => {
+        assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+    });
+});
+  
