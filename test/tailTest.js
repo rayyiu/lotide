1ac73703219f7606b1c7ea085f332ff02f382091
Ray <rayyiu66@gmail.com>
2021-02-08 19:03:40 -0800

    refactored tail.js with mocha

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,23 @@
 const tail = require("../tail");
-const assertEqual = require("../assertEqual")
+const assert = require('chai').assert;
 
-console.log(tail([1, 2, 3, 4, 5]));
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-console.log(assertEqual(result, ["Lighthouse", "Labs"]));
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log("tail,", tail(words));
+describe("#tail", () => {
+    it("returns 2, 3 for [1, 2, 3]", () => {
+      assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+    });
+    it("returns [] for ['5']", () => {
+      assert.deepEqual(tail(['5']), []); 
+    });
+    it("returns ['Lighthouse', 'Labs'] for [['Hello', 'Lighthouse', 'Labs']", () => {
+        assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"])
+    });
+});
+  
+
+
+
+// console.log(tail([1, 2, 3, 4, 5]));
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// console.log(assertEqual(result, ["Lighthouse", "Labs"]));
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// console.log("tail,", tail(words));
