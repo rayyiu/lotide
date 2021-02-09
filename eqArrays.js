e8fc6ee3566243e6989c2287b5e1e6d5550c840b
Ray <rayyiu66@gmail.com>
2021-02-08 16:02:58 -0800

    refactored eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -7,12 +7,4 @@ const eqArrays = (array1, array2) => {
 return true;
 }
 
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual}  === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual}  !==  ${expected}`);
-  }
-};
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
-    
+const assertEqual = require('assertEqual')
